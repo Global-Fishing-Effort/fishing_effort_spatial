28ef43a140625b31e7fec9a1ec58de2a6bc1f10b
Ubuntu <ubuntu@gage-rstudio.novalocal>
2024-09-09 20:53:21 +0000

    fixed trawling proportions

--- R/spatial_files.R
+++ R/spatial_files.R
@@ -61,17 +61,38 @@ eez_rast
 
 eez_rast[is.na(eez_rast)] <- 99999 # save a value high seas, not if rousseau has that in there..
 eez_rast <- mask(rast(eez_rast), vect(world_shp), inverse = TRUE) # mask out any land 
-eez_rast[is.na(eez_rast)] <- 999999 # save a value high seas, not if rousseau has that in there..
+eez_rast[is.na(eez_rast)] <- 999999 # save a value land, not if rousseau has that in there..
 
 
 writeRaster(eez_rast, here("raw/spatial/eez_id_rast.tif"), overwrite = TRUE)
+
 
 ## get x, y lookup table so we can join to the raw mmsi data when saving and have the EEZ id already saved in the csv 
 
 eez_lookup <- eez_rast %>%
   as.data.frame(xy = TRUE) %>%
   rename(mrgid = layer) %>%
-  mutate(x = round(x, 1), y = round(y, 1))
+  # mutate(x = round(x, 1), y = round(y, 1))
+  mutate(x = ifelse(x > 0, ceiling(x * 10) / 10, floor(x * 10) / 10),
+         y = ifelse(y > 0, ceiling(y * 10) / 10, floor(y * 10) / 10))
+
+eez_check <- eez_lookup %>%
+  mutate(x_int = floor(x), y_int = floor(y)) %>%  # extract integer part of x and y
+  group_by(x_int, y_int) %>%
+  summarise(x_unique_decimals = n_distinct(x), 
+            y_unique_decimals = n_distinct(y)) %>%
+  ungroup()
+
+# Check for any integers that do not have exactly 10 decimal versions
+missing_decimals <- eez_check %>%
+  filter(x_unique_decimals != 10 | y_unique_decimals != 10) # 0 rows - perfect! 
+
+test <- eez_lookup %>%
+  filter(is.na(mrgid)) # ok no NAs in mrgid that's good
+# is it really supposed to have 6.4 million cells? Yes. 
+
+test_flk <- eez_lookup %>%
+  filter(mrgid == 8389)
 
 qs::qsave(eez_lookup, here("raw/spatial/eez_rast_csv.qs"))
-## ok i'm not sure this is right... but oh well. 
+## every point i've looked up seems right... 
